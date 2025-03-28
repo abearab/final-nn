fix: correct sample count calculation in loss functions

nn/nn.py
@@ -456,7 +456,7 @@ class NeuralNetwork:
             loss: float
                 Average loss of mini-batch.
         """
-        m = y.shape[1] # number of samples
+        m = y.shape[0] # number of samples
 
         # Compute the mean squared error
         loss = np.sum((y - y_hat) ** 2) / (2 * m)
@@ -477,7 +477,7 @@ class NeuralNetwork:
             dA: ArrayLike
                 partial derivative of loss with respect to A matrix.
         """
-        m = y.shape[1] # number of samples
+        m = y.shape[0] # number of samples
 
         # Compute the derivative of the mean squared error
         dA = (2 / m) * (y_hat - y)